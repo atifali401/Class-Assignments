be654bddfda2f4c74d9e4e9fe967912c21ca6ced
Atif Ali <160436242+atifali401@users.noreply.github.com>
2024-03-06 15:05:11 -0800

    Add files via upload

---
+++ Test Folder/Grading System.ts
@@ -0,0 +1,22 @@
+// Task for Grading on basises of marks or precent % 
+
+
+ let marks  = 90;
+  let grade;
+   if(marks>=90 && marks <=100){grade = 'A*'}
+
+   else if(marks>=80 && marks<=89)
+   {grade='A'}
+
+   else if(marks>=70 && marks<=69)
+   {grade='B'}
+
+   else if(marks>=60 && marks<=59)
+   {grade='C'}
+
+   else if(marks>=50 && marks<=49)
+   {grade='D'}
+
+   else if(marks>=40 && marks<=39)
+   {grade='E'};
+   console.log("According to grade", grade);
